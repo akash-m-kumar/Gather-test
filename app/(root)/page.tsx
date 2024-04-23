d60828d6f706164909afc76aea93462fd984dc4d
Akash M Kumar <akashmkumar1@gmail.com>
2024-04-23 09:31:16 +0530

    minor changes to data

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -25,7 +25,7 @@ export default async function Home({ searchParams }: SearchParamProps) {
         <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
           <div className="flex flex-col justify-center gap-8">
             <h1 className="h1-bold">All your events at one place</h1>
-            <p className="p-regular-20 md:p-regular-24">Book and learn helpful tips from 3,168+ mentors in world-class companies with our global community.</p>
+            <p className="p-regular-20 md:p-regular-24">Book and learn helpful tips from thousands of mentors in world-class companies with our global community.</p>
             <Button size="lg" asChild className="button w-full sm:w-fit">
               <Link href="#events">
                 Explore Now
@@ -44,7 +44,7 @@ export default async function Home({ searchParams }: SearchParamProps) {
       </section> 
 
       <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
-        <h2 className="h2-bold">Trusted by <br /> Thousands of Events Managers.</h2>
+        <h2 className="h2-bold">Trusted by <br/> Thousands of Events Managers.</h2>
 
         <div className="flex w-full flex-col gap-5 md:flex-row">
           <Search />
